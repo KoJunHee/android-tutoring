e6f537d9d19b8a1f9bc2ff1e454a4dbd4089e34a
jay <kjh338076@naver.com>
2018-12-07 13:54:33 +0900

    add listview code

--- ListViewApp/app/src/main/java/com/example/junhee/listviewapp/MainActivity.java
+++ ListViewApp/app/src/main/java/com/example/junhee/listviewapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.junhee.listviewapp;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
 import java.util.ArrayList;
